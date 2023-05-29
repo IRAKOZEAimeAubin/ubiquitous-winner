73591a17c42886e97ffa437bfccf30f10489d9df
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2023-05-29 15:45:49 +0200

    chore: initial setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex flex-col w-[100%] items-center">
+    <h1>Hello</h1>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: [ "'Lora', serif", ...defaultTheme.fontFamily.serif ],
+        sans: [ "'Montserrat', sans-serif", ...defaultTheme.fontFamily.sans ],
+        mono: [ "'DM Mono', monospace", ...defaultTheme.fontFamily.mono ]
+      }
+    },
+  },
+  plugins: [],
+};
