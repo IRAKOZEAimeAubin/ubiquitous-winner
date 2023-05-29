68c50049f6bbcd7fd40501edb38c691e31322d52
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2023-05-29 18:01:17 +0200

    feat: add building blocks of the reaction timer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
 </script>
 
 <template>
-  <div class="flex flex-col w-[100%] items-center">
-    <h1>Hello</h1>
+  <div class="flex flex-col gap-4 w-[100%] items-center mt-8">
+    <h1 class="text-4xl font-bebas tracking-widest font-bold text-red-600">Reaction Timer</h1>
+      <button class="uppercase tracking-wider font-semibold text-white bg-green-600 py-1 px-3 rounded-md hover:opacity-80" @click=" start ">Play</button>
+    <Results/>
+    <Block v-if="isPlaying"/>
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    Block, Results
+  },
+  data () {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start () {
+      this.delay = Math.floor( Math.random() * 1000 ) + 1000;
+      this.isPlaying = true;
+      console.log( this.delay );
+    }
+  }
+}
+</script>
---
+++ src/components/Block.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="uppercase text-orange-400 font-mono font-black text-center w-[400px] bg-gray-950 rounded-md py-24">Click Me</div>
+</template>
+
+<script>
+export default {
+    name: "Block",
+    props: [
+        "delay"
+    ]
+}
+</script>
