365d4c6dd29ae31dff0ecd3465ac16c2a260defe
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2023-05-30 16:22:42 +0200

    feat: add finishing touches

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ import Results from './components/Results.vue';
 <template>
   <div class="flex flex-col gap-4 w-[100%] items-center mt-8">
     <h1 class="text-4xl font-bebas tracking-widest font-bold text-red-600">Reaction Timer</h1>
-      <button class="uppercase tracking-wider font-semibold text-white bg-green-600 py-1 px-3 rounded-md hover:opacity-80 disabled:opacity-40" @click=" start " :disabled="isPlaying">Play</button>
+    <p class="bg-orange-100 px-2 py-1 rounded-md">Click on the <code class="font-bold">PLAY</code> button and wait for a <code class="font-bold">BLACK</code> rectangle to appear, the click on it.</p>
+    <button class="uppercase tracking-wider font-semibold text-white bg-green-600 py-1 px-3 rounded-md hover:opacity-80 disabled:opacity-40" @click=" start " :disabled="isPlaying">Play</button>
     <Results v-if="showResults" :score="score"/>
     <Block v-if="isPlaying" :delay="delay" @stop="endGame"/>
   </div>
